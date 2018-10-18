1036e8b633782591970f40207730190bd9457713
CloudCannon <support@cloudcannon.com>
2018-10-18 22:50:15 +0000

    Updated 192 files via CloudCannon

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# Medium Rare Books
-A Jekyll microsite for the library of Intercom Brand Studio.
--- _site/js/application.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-      $('#search-input').blur(function() {
-              $('.max-container').removeClass("focus");
-            })
-            .focus(function() {
-              $('.max-container').addClass("focus")
-            });
-});
