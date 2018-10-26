d130531d5e586e7accfd2585d0443ad7d153ef8e
CloudCannon <support@cloudcannon.com>
2018-10-26 21:55:59 +0000

    Updated 77 files via CloudCannon

--- js/application.js
+++ js/application.js
@@ -11,4 +11,23 @@ $(document).ready(function () {
               resultsContainer: document.getElementById('results-container'),
               json: '/search.json'
       })
+      const bookCarousel = new Siema({
+        selector: '.siema',
+        easing: 'ease',
+        perPage: 1,
+        startIndex: 0,
+        draggable: false,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {
+             var page = bookCarousel.currentSlide + 1;
+             document.querySelector('.siema__nav-pagination .page').innerHTML = page;
+       },
+      });
+
+      document.querySelector('.siema__nav-prev').addEventListener('click', () => bookCarousel.prev());
+      document.querySelector('.siema__nav-next').addEventListener('click', () => bookCarousel.next());
 });
