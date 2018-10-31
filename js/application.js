1eef9b612ad3086a6a458956ce46a27f1343ba32
CloudCannon <support@cloudcannon.com>
2018-10-31 22:59:37 +0000

    Updated 166 files via CloudCannon

--- js/application.js
+++ js/application.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
       $('#search-input').blur(function() {
               $('.max-container').removeClass("focus");
-            })
-            .focus(function() {
-              $('.max-container').addClass("focus")
+            }).focus(function() {
+              $('.max-container').addClass("focus");
             });
 
       SimpleJekyllSearch({
